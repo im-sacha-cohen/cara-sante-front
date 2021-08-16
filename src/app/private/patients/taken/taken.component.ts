263f8bbcd098a673f11523633f6e67260a83eef1
Sacha COHEN <contact@sacha-cohen.fr>
2021-08-16 17:43:34 +0200

    Taken patient

--- src/app/private/patients/taken/taken.component.ts
+++ src/app/private/patients/taken/taken.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { QueryService } from 'src/app/shared/services/query/query.service';
 
 @Component({
   selector: 'app-taken',
@@ -6,10 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./taken.component.scss']
 })
 export class TakenPatientComponent implements OnInit {
+  showSpinner = true;
+  patients: any;
 
-  constructor() { }
+  constructor(
+    private queryService: QueryService
+  ) { }
 
   ngOnInit(): void {
+    this.getTakenPatient();
   }
 
+  getTakenPatient(): void {
+    this.queryService.query(
+      'GET',
+      '/api/patient/taken'
+    ).subscribe(
+      patients => {
+        this.patients = patients;
+        console.log(this.patients);
+        this.showSpinner = false;
+      },
+      error => {
+        //
+        this.showSpinner = false;
+      }
+    );
+  }
 }
