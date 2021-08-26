0c66568cd28814588e3dc8b0b2f90e757dd798bd
Sacha COHEN <contact@sacha-cohen.fr>
2021-08-26 13:15:01 +0200

    Add user & UI

--- src/app/private/user/add/add.component.ts
+++ src/app/private/user/add/add.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { QueryService } from 'src/app/shared/services/query/query.service';
 
 @Component({
   selector: 'app-add-user',
@@ -6,10 +8,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add.component.scss']
 })
 export class AddUserComponent implements OnInit {
+  addUserForm: FormGroup;
+  showSpinner = false;
+  isError = false;
+  errorMessage: string;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private queryService: QueryService
+  ) { }
 
   ngOnInit(): void {
+    this.buildForm();
   }
 
+  buildForm(): void {
+    this.addUserForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', Validators.required],
+      phone: ['', Validators.required]
+    });
+  }
+
+  onClick(): void {
+    this.showSpinner = true;
+    this.isError = false;
+
+    this.queryService.query(
+      'POST',
+      '/api/user',
+      this.addUserForm.value
+    ).subscribe(
+      resp => {
+        this.showSpinner = false;
+      },
+      error => {
+        this.showSpinner = false;
+        this.isError = true;
+        this.errorMessage = error.error.message;
+        console.log(error);
+      }
+    );
+  }
 }
