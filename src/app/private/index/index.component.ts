e6c04ddb53ab999760ce3a0743d2d222ee124d42
Sacha COHEN <contact@sacha-cohen.fr>
2021-09-17 13:52:14 +0200

    Add index user stat

--- src/app/private/index/index.component.ts
+++ src/app/private/index/index.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ChartDataset, ChartType } from 'chart.js';
 import { AuthService } from 'src/app/shared/services/auth/service/auth-service.service';
+import { QueryService } from 'src/app/shared/services/query/query.service';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-index',
@@ -8,19 +10,58 @@ import { AuthService } from 'src/app/shared/services/auth/service/auth-service.s
   styleUrls: ['./index.component.scss']
 })
 export class IndexComponent implements OnInit {
-  public barChartLabels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+  public barChartLabels: any[];
   public barChartType: ChartType = 'bar';
   public barChartLegend = true;
   public barChartPlugins = [];
+  public barChartData: ChartDataset[];
+
+  showUserStatSpinner = false;
   firstName = '';
 
-  public barChartData: ChartDataset[] = [
-    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Nombre de patients saisis' }
-  ];
-
-  constructor(private authService: AuthService) { }
+  constructor(
+    private authService: AuthService,
+    private queryService: QueryService
+  ) { }
 
   ngOnInit(): void {
     this.firstName = this.authService.getFirstName();
+    this.getStats();
+  }
+
+  getStats(): void {
+    this.showUserStatSpinner = true;
+
+    this.queryService.query(
+      'GET',
+      '/api/user/get-stats'
+    ).subscribe(
+      (resp: any[]) => {
+        this.buildUserStat(resp);
+        this.showUserStatSpinner = false;
+      }
+    );
+  }
+
+  buildUserStat(resp: any): void {
+    const stats = _.values(resp);
+    const chartData = [];
+    const chartLabels = [];
+
+    stats.forEach(element => {
+      const el = _.values(element.object);
+      chartData.push(el.length);
+      chartLabels.push(element.dateText);
+    });
+
+    chartData.reverse();
+    chartLabels.reverse();
+
+    this.barChartData = [{
+      data: chartData,
+      label: 'Nombre de patients saisis' }
+    ];
+
+    this.barChartLabels = chartLabels;
   }
 }
