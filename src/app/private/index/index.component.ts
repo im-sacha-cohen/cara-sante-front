22cfa70e652d3475ccdb98f0bdbb9aea8427d7c8
Sacha COHEN <contact@sacha-cohen.fr>
2021-09-17 15:06:47 +0200

    Team stats

--- src/app/private/index/index.component.ts
+++ src/app/private/index/index.component.ts
@@ -10,13 +10,17 @@ import * as _ from 'lodash';
   styleUrls: ['./index.component.scss']
 })
 export class IndexComponent implements OnInit {
-  public barChartLabels: any[];
+  public barChartUserLabels: any[];
   public barChartType: ChartType = 'bar';
   public barChartLegend = true;
   public barChartPlugins = [];
-  public barChartData: ChartDataset[];
+  public barChartUserData: ChartDataset[];
+
+  public barChartTeamLabels: any[];
+  public barChartTeamData: ChartDataset[];
 
   showUserStatSpinner = false;
+  showTeamStatSpinner = false;
   firstName = '';
 
   constructor(
@@ -26,19 +30,34 @@ export class IndexComponent implements OnInit {
 
   ngOnInit(): void {
     this.firstName = this.authService.getFirstName();
-    this.getStats();
+    this.getUserStats();
+    this.getTeamStats();
   }
 
-  getStats(): void {
+  getUserStats(): void {
     this.showUserStatSpinner = true;
 
     this.queryService.query(
       'GET',
-      '/api/user/get-stats'
+      '/api/user/get-user-stats'
     ).subscribe(
       (resp: any[]) => {
         this.buildUserStat(resp);
         this.showUserStatSpinner = false;
+      }
+    );
+  }
+
+  getTeamStats(): void {
+    this.showTeamStatSpinner = true;
+
+    this.queryService.query(
+      'GET',
+      '/api/user/get-team-stats'
+    ).subscribe(
+      (resp: any[]) => {
+        this.buildTeamStat(resp);
+        this.showTeamStatSpinner = false;
       }
     );
   }
@@ -57,11 +76,33 @@ export class IndexComponent implements OnInit {
     chartData.reverse();
     chartLabels.reverse();
 
-    this.barChartData = [{
+    this.barChartUserData = [{
       data: chartData,
-      label: 'Nombre de patients saisis' }
+      label: 'Nombre de patients saisit par vous-même' }
     ];
 
-    this.barChartLabels = chartLabels;
+    this.barChartUserLabels = chartLabels;
+  }
+
+  buildTeamStat(resp: any): void {
+    const stats = _.values(resp);
+    const chartData = [];
+    const chartLabels = [];
+
+    stats.forEach(element => {
+      const el = _.values(element.object);
+      chartData.push(el.length);
+      chartLabels.push(element.dateText);
+    });
+
+    chartData.reverse();
+    chartLabels.reverse();
+
+    this.barChartTeamData = [{
+      data: chartData,
+      label: 'Nombre de patients saisit par toute l\'équipe' }
+    ];
+
+    this.barChartTeamLabels = chartLabels;
   }
 }
