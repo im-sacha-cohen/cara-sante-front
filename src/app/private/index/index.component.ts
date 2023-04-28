a4032a58ce3ae3c4b2d3e46c5f09fdba24169886
Sacha COHEN <sacha@metamundo.co>
2023-04-28 10:57:41 +0200

    Add erning graph

--- src/app/private/index/index.component.ts
+++ src/app/private/index/index.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { ChartDataset, ChartType } from 'chart.js';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Chart, ChartConfiguration, ChartDataset, ChartType } from 'chart.js';
 import { AuthService } from 'src/app/shared/services/auth/service/auth-service.service';
 import { QueryService } from 'src/app/shared/services/query/query.service';
 import * as _ from 'lodash';
 import { Title } from '@angular/platform-browser';
+import { BaseChartDirective } from 'ng2-charts';
+import { default as Annotation } from 'chartjs-plugin-annotation';
 
 @Component({
   selector: 'app-index',
@@ -28,13 +30,85 @@ export class IndexComponent implements OnInit {
   isAdmin = false;
   earning: any;
   showEarningSpinner = true;
+  showEarningChartSpinner = true;
+
+  public lineChartData: ChartConfiguration['data'] = {
+    datasets: [
+      {
+        data: [ ],
+        label: 'Gains en €',
+        backgroundColor: 'rgba(148,159,177,0.2)',
+        borderColor: '#007b91',
+        pointBackgroundColor: '#ffa4b7',
+        pointBorderColor: 'green',
+        pointHoverBackgroundColor: '#fff',
+        pointHoverBorderColor: 'rgba(148,159,177,0.8)',
+        fill: 'origin'
+      }
+    ],
+    labels: [ ]
+  };
+
+  public lineChartOptions: ChartConfiguration['options'] = {
+    elements: {
+      line: {
+        tension: 0.5
+      }
+    },
+    scales: {
+      // We use this empty structure as a placeholder for dynamic theming.
+      y:
+        {
+          position: 'left',
+        },
+      y1: {
+        position: 'right',
+        grid: {
+          color: '#007b91',
+        },
+        ticks: {
+          color: '#007b91'
+        }
+      }
+    },
+
+    plugins: {
+      legend: { display: true },
+      annotation: {
+        annotations: [
+          {
+            type: 'line',
+            scaleID: 'x',
+            value: '',
+            borderColor: '#ffa4b7',
+            borderWidth: 2,
+            label: {
+              display: true,
+              position: 'center',
+              color: 'white',
+              backgroundColor: '#007b91',
+              content: `Gains de ${this.authService.getFirstName()}`,
+              font: {
+                weight: 'bold'
+              }
+            }
+          },
+        ],
+      }
+    }
+  };
+
+  public lineChartType: ChartType = 'line';
+
+  @ViewChild(BaseChartDirective) chart?: BaseChartDirective;
 
   constructor(
     private authService: AuthService,
     private queryService: QueryService,
-    private title: Title
+    private title: Title,
   ) {
     this.title.setTitle('Liora | Cara Santé - Accueil');
+    Chart.register(Annotation)
   }
 
   ngOnInit(): void {
@@ -43,6 +117,7 @@ export class IndexComponent implements OnInit {
     this.getTeamStats();
     this.getDetectionTestRemaining();
     this.getEarning();
+    this.getEarningChart();
     this.isAdmin = this.authService.isAdmin();
   }
 
@@ -103,6 +178,22 @@ export class IndexComponent implements OnInit {
     );
   }
 
+  getEarningChart(): void {
+    this.showEarningChartSpinner = true;
+
+    this.queryService.query(
+      'GET',
+      '/api/user/earning-chart'
+    ).subscribe(
+      resp => {
+        this.showEarningChartSpinner = false;
+        this.lineChartData.datasets[0].data = resp.data;
+        this.lineChartData.labels = resp.labels;
+      },
+      () => this.showEarningChartSpinner = false
+    );
+  }
+
   buildUserStat(resp: any): void {
     const stats = _.values(resp);
     const chartData = [];
@@ -119,7 +210,7 @@ export class IndexComponent implements OnInit {
 
     this.barChartUserData = [{
       data: chartData,
-      label: 'Nombre de patients saisis par vous' }
+      label: 'Nombre de patients que j\'ai saisi' }
     ];
 
     this.barChartUserLabels = chartLabels;
